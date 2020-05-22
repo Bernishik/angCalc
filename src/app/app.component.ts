594f6e2c8c360022293f5fda54a198405e4bfdc6
Bernish <hanychak@gmail.com>
2020-05-22 11:27:17 +0300

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angcalk';
+ 
+
+  calcForm: any = {
+    first: 0,
+    second: 0
+  }
+
+  add(first,second){
+    return first + second;
+  }
+  remove(first,second){
+    return first - second;
+  }
+
+  mul(first,second){
+    return first * second;
+  }
+  div(first,second){
+    if(second == 0){
+      return "Error. Cannot divide by 0";
+    }else{
+      return first / second;
+    }
+  }
+
+
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FormsModule } from '@angular/forms';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { CommonModule } from '@angular/common';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { ModalModule } from 'ngx-bootstrap/modal';
+// import { AppBootstrapModule } from './app-bootstrap/app-bootstrap.module';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    // AppBootstrapModule,
+    BsDropdownModule.forRoot(),
+    TooltipModule.forRoot(),
+    ModalModule.forRoot()
   ],
+  exports: [BsDropdownModule, TooltipModule, ModalModule],
   providers: [],
   bootstrap: [AppComponent]
 })
